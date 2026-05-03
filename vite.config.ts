00a3ef6ef7b83b773b8b57a345cb047dc3d31c4b
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2026-05-03 17:12:46 +0530

    minor fix

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://fd-manager-4exx.onrender.com/',
         changeOrigin: true,
       },
     },
