d5d4f3d4db53158d1180b076fb1e18261fbc0541
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2026-05-03 17:56:21 +0530

    minor fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,24 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'path'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://fd-manager-4exx.onrender.com/',
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
